59aea6062d569f7db12bec2b4fff196a04430002
sonam-yoezer <forbusinesssjy999@gmail.com>
2024-11-20 11:42:25 +0600

    MTS-01: Initialization Project

---
+++ src/main/java/com/java/movieticketingsystem/MovieTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java.movieticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketingSystemApplication.class, args);
+	}
+
+}

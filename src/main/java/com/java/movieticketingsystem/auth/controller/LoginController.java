63cab08b22b90384a945e8b42361a5805417ebb1
rigyel-dev <152839920+rigyel-dev@users.noreply.github.com>
2024-12-05 11:18:30 +0600

    MTS08: Implemented Login

---
+++ src/main/java/com/java/movieticketingsystem/auth/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.java.movieticketingsystem.auth.controller;
+
+import com.java.movieticketingsystem.auth.model.AuthRequest;
+import com.java.movieticketingsystem.auth.service.LoginService;
+import com.java.movieticketingsystem.utils.RestHelper;
+import com.java.movieticketingsystem.utils.RestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api/v1/login")
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    /**
+     * Handles the authentication for the user provided credentials.
+     *
+     * @param authRequest The authentication credentials containing object
+     * @return The access keys and refresh keys for the associated authenticated user.
+     */
+    @PostMapping
+    public ResponseEntity<RestResponse> login(@RequestBody AuthRequest authRequest) {
+        HashMap<String, Object> listHashMap = new HashMap<>();
+        listHashMap.put("access_token",     loginService.authenticate(authRequest));
+        return RestHelper.responseSuccess(listHashMap);
+    }
+}
